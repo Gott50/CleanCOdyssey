2a58a11759ded098cdd5578a83747b7b6f0124ed
Gott50 <xxGott50xx@gmail.com>
2016-07-02 16:17:22 +0200

    day 2 gildedrose

--- Katas/src/gildedrose/GildedRose.java
+++ Katas/src/gildedrose/GildedRose.java
@@ -7,56 +7,85 @@ class GildedRose {
         this.items = items;
     }
 
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateQuality(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            updateSellIn(item);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstagePass(item)) {
+                        if (item.quality > 0) {
+                            if (!isSulfuras(item)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    updateQualityAgedBrie(item);
                 }
             }
         }
+    }
+
+    private void updateQualityAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void updateQuality(Item item) {
+        if (!isAgedBrie(item)
+                && !isBackstagePass(item)) {
+            if (item.quality > 0) {
+                if (!isSulfuras(item)) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            updateAgedBrie(item);
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (isBackstagePass(item)) {
+                updateBackstagePass(item);
+            }
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        if (item.sellIn < 11) {
+            updateQualityAgedBrie(item);
+        }
+
+        if (item.sellIn < 6) {
+            updateQualityAgedBrie(item);
+        }
+    }
+
+    private void updateSellIn(Item item) {
+        if (!isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
     }
 }
--- Katas/src/gildedrose/GildedRoseTest.java
+++ Katas/src/gildedrose/GildedRoseTest.java
@@ -5,91 +5,147 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-	 class ItemBuilder{
-		private String name;
-		private int quality;
-		private int sellIn;
+    private class ItemBuilder {
+        private String name;
+        private int quality;
+        private int sellIn;
 
-		public ItemBuilder() {
-			name = "+5 Dexterity Vest";
-			sellIn = 10;
-			quality = 20;
-		}
-		public ItemBuilder withName(String name) {
-			this.name = name;
-			return this;
-		}
-		public ItemBuilder withQuality(int quality) {
-			this.quality = quality;
-			return this;
-		}
-		public ItemBuilder withSellIn(int sellIn) {
-			this.sellIn = sellIn;
-			return this;
-		}
-		public Item build(){
-			return new Item(name, sellIn, quality);
-		}
-	}
+        ItemBuilder() {
+            name = "+5 Dexterity Vest";
+            sellIn = 15;
+            quality = 20;
+        }
 
-	@Test
-	public void testNormalItemName() {
-		Item[] items = new Item[] { new ItemBuilder().withName("foo").build()};
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("foo", app.items[0].name);
-	}
+        ItemBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
 
-	@Test
-	public void testNormalItemUpdateSellIn() {
-		Item[] items = new Item[] { new ItemBuilder().withSellIn(10).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(9, app.items[0].sellIn);
-	}
+        ItemBuilder withQuality(int quality) {
+            this.quality = quality;
+            return this;
+        }
 
-	@Test
-	public void testNormalItemUpdateQuality() {
-		Item[] items = new Item[] { new ItemBuilder().withQuality(20).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(19, app.items[0].quality);
-	}
-	
-	@Test
-	public void testNormalItemPassedSellIn() {
-		Item[] items = new Item[] { new ItemBuilder().withSellIn(0).withQuality(20).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(18, app.items[0].quality);
-	}
-	@Test
-	public void testNormalItemQualityAlwaysPositive() {
-		Item[] items = new Item[] { new ItemBuilder().withSellIn(0).withQuality(0).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(0, app.items[0].quality);
-	}
-	@Test
-	public void testAgedBrieUpdateQuality() {
-		Item[] items = new Item[] { new ItemBuilder().withName("Aged Brie").withQuality(20).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(21, app.items[0].quality);
-	}
-	@Test
-	public void testAgedBrieUpdateQualityLess51() {
-		Item[] items = new Item[] { new ItemBuilder().withName("Aged Brie").withQuality(50).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertTrue(app.items[0].quality < 51);
-	}
-	@Test
-	public void testSulfurasUpdateQuality() {
-		Item[] items = new Item[] { new ItemBuilder().withName("Sulfuras, Hand of Ragnaros").withQuality(80).build() };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(80, app.items[0].quality);
-	}
+
+        ItemBuilder withSellIn(int sellIn) {
+            this.sellIn = sellIn;
+            return this;
+        }
+
+        Item build() {
+            return new Item(name, sellIn, quality);
+        }
+    }
+
+    @Test
+    public void testItemName() {
+        Item[] items = new Item[]{new ItemBuilder().withName("foo").build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+    @Test
+    public void testItemToString() {
+        Item[] items = new Item[]{new ItemBuilder().withName("+5 Dexterity Vest").withSellIn(10).withQuality(20).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("+5 Dexterity Vest, 9, 19", app.items[0].toString());
+    }
+
+    @Test
+    public void testItemUpdateSellIn() {
+        Item[] items = new Item[]{new ItemBuilder().withSellIn(10).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    public void testItemUpdateQuality() {
+        Item[] items = new Item[]{new ItemBuilder().withQuality(20).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19, app.items[0].quality);
+    }
+
+    @Test
+    public void testItemPassedSellIn() {
+        Item[] items = new Item[]{new ItemBuilder().withSellIn(0).withQuality(20).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(18, app.items[0].quality);
+    }
+
+    @Test
+    public void testItemQualityAlwaysPositive() {
+        Item[] items = new Item[]{new ItemBuilder().withSellIn(0).withQuality(0).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void testAgedBrieUpdateQuality() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Aged Brie").withQuality(20).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    public void testAgedBrieUpdateQualityWithSellInLessThan0() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Aged Brie").withSellIn(-1).withQuality(20).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+    }
+
+    @Test
+    public void testAgedBrieUpdateQualityLess51() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Aged Brie").withQuality(50).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality < 51);
+    }
+
+    @Test
+    public void testSulfurasUpdateQuality() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Sulfuras, Hand of Ragnaros").withQuality(80).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstagePassUpdateQuality() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Backstage passes to a TAFKAL80ETC concert").withQuality(10).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstagePassUpdateQualityWithSellInLessThan10() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Backstage passes to a TAFKAL80ETC concert").withQuality(10).withSellIn(9).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstagePassUpdateQualityWithSellInLessThan5() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Backstage passes to a TAFKAL80ETC concert").withQuality(10).withSellIn(4).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstagePassUpdateQualityAfterConcert() {
+        Item[] items = new Item[]{new ItemBuilder().withName("Backstage passes to a TAFKAL80ETC concert").withQuality(10).withSellIn(0).build()};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
 
 }
