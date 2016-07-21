1fcf82f5585988568ae1d017c61db0420cd0d35a
Gott50 <xxGott50xx@gmail.com>
2016-07-21 21:07:11 +0200

    day 1 BoundedQueue

--- Katas/src/Bounded_Queue/solution1/BoundedQueue.java
+++ Katas/src/Bounded_Queue/solution1/BoundedQueue.java
@@ -2,16 +2,25 @@ package Bounded_Queue.solution1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class BoundedQueue<T> {
     private final int size;
-    private ArrayList<T> queue;
+    private final ArrayList<T> queue;
+    private final ArrayList<T> comingIn;
+    private final Lock readLock;
+    private final Lock writeLock;
 
 
     public BoundedQueue(int size) {
+        readLock = new ReentrantLock();
+        readLock.lock();
+        writeLock = new ReentrantLock();
         this.size = size;
-        if (queue == null)
-            queue = new ArrayList<>();
+        queue = new ArrayList<>();
+        comingIn = new ArrayList<>();
     }
 
     int getSize() {
@@ -19,22 +28,51 @@ class BoundedQueue<T> {
     }
 
     void enqueue(T value) {
-        queue.add(value);
-
+        comingIn.add(value);
+        updateQueue();
     }
+
+    synchronized private void updateQueue() {
+        if (count() != size)
+            if (comingIn.size() > 0) {
+                queue.add(getNextValue());
+                if (!readLock.tryLock())
+                    readLock.unlock();
+            }
+    }
+
 
     List<T> values() {
         return queue;
     }
 
     T dequeue() {
-        int lastIndex = queue.size() - 1;
-        T out = queue.get(lastIndex);
-        queue.remove(lastIndex);
+        T out = null;
+        try {
+            if (readLock.tryLock(10, TimeUnit.SECONDS)) {
+                if (count() == 0) readLock.lock();
+                else {
+                    out = queue.get(0);
+                    queue.remove(0);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (!writeLock.tryLock())
+                writeLock.unlock();
+            updateQueue();
+        }
         return out;
     }
 
     int count() {
         return values().size();
+    }
+
+    private T getNextValue() {
+        T nextValue = comingIn.get(0);
+        comingIn.remove(0);
+        return nextValue;
     }
 }
--- Katas/src/Bounded_Queue/solution1/BoundedQueueTest.java
+++ Katas/src/Bounded_Queue/solution1/BoundedQueueTest.java
@@ -3,18 +3,45 @@ package Bounded_Queue.solution1;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class BoundedQueueTest {
-    private final int size = 2;
     private BoundedQueue boundedQueue;
+    private WritingThread<Integer> writhing;
+    private ReadingThread reading;
 
     @Before
     public void setUp() throws Exception {
+        int size = 2;
         boundedQueue = new BoundedQueue<Integer>(size);
+        writhing = new WritingThread<>(1, 2, 3, 4, 5);
+        reading = new ReadingThread();
+    }
+
+    @Test
+    public void integration() {
+        writhing.run();
+        assertEquals(((List) Arrays.asList(1)).toString(), boundedQueue.values().toString());
+        reading.run();
+        writhing.run();
+        assertEquals(((List) Arrays.asList(2)).toString(), boundedQueue.values().toString());
+        writhing.run();
+        assertEquals(((List) Arrays.asList(2, 3)).toString(), boundedQueue.values().toString());
+        writhing.run();
+        reading.run();
+        assertEquals(((List) Arrays.asList(3, 4)).toString(), boundedQueue.values().toString());
+        reading.run();
+        assertEquals(((List) Arrays.asList(4)).toString(), boundedQueue.values().toString());
+        reading.run();
+        reading.run();
+        writhing.run();
+        assertEquals(((List) Arrays.asList(5)).toString(), boundedQueue.values().toString());
+        reading.run();//unnecessary
+        assertEquals(5, reading.lastOutput);
     }
 
     @Test
@@ -38,11 +65,6 @@ public class BoundedQueueTest {
     }
 
     @Test
-    public void Enqueue_Block() {
-        assertEquals(true, false);
-    }
-
-    @Test
     public void Dequeue() {
         int value = 1;
         boundedQueue.enqueue(value);
@@ -51,16 +73,63 @@ public class BoundedQueueTest {
     }
 
     @Test
-    public void Dequeue_Block() {
-        assertEquals(true, false);
+    public void Dequeue_givenTwoElements() {
+        boundedQueue.enqueue(1);
+        boundedQueue.enqueue(2);
+        assertEquals(1, boundedQueue.dequeue());
+        assertEquals(1, boundedQueue.values().size());
     }
 
     @Test
     public void Count() {
         boundedQueue.enqueue(1);
         boundedQueue.enqueue(2);
+        assertEquals(2, boundedQueue.count());
+    }
+
+    @Test
+    public void Enqueue_BlockToMuch() {
+        boundedQueue.enqueue(1);
+        boundedQueue.enqueue(2);
         boundedQueue.enqueue(3);
-        assertEquals(3, boundedQueue.count());
+        List expected = Arrays.asList(1, 2);
+        assertEquals(expected.toString(), boundedQueue.values().toString());
+    }
+
+    @Test
+    public void Enqueue_ReleaseBlock() {
+        boundedQueue.enqueue(1);
+        boundedQueue.enqueue(2);
+        boundedQueue.enqueue(3);
+        boundedQueue.dequeue();
+        List expected = Arrays.asList(2, 3);
+        assertEquals(expected.toString(), boundedQueue.values().toString());
+    }
+
+    class WritingThread<T> extends Thread {
+        final ArrayList<T> input;
+
+        public WritingThread(T... input) {
+            this.input = new ArrayList(Arrays.asList(input));
+        }
+
+        @Override
+        public void run() {
+            T value = input.get(0);
+            input.remove(0);
+            boundedQueue.enqueue(value);
+        }
+    }
+
+    class ReadingThread extends Thread {
+        final ArrayList output = new ArrayList();
+        Object lastOutput;
+
+        @Override
+        public void run() {
+            lastOutput = boundedQueue.dequeue();
+            output.add(lastOutput);
+        }
     }
 
 
