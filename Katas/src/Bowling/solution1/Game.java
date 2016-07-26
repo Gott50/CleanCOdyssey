75673b254fbc09fb2618964336812afe954a7b01
Gott50 <xxGott50xx@gmail.com>
2016-07-26 20:54:20 +0200

    day 2 Bowling

--- Katas/src/Bowling/solution1/Game.java
+++ Katas/src/Bowling/solution1/Game.java
@@ -11,27 +11,51 @@ class Game {
     }
 
     boolean isOver() {
-        return frames[9] != null && rollIndex >= 19;
+        if (frames[9] != null) {
+            if (rollIndex > 20) return true;
+            if (isSpare(frames[9])) return false;
+            if (rollIndex >= 20) return true;
+
+        }
+        return false;
     }
 
-    void addRoll(int pins) {
-        int index = rollIndex / 2;
-        if (rollIndex % 2 == 0)
-            frames[index] = new Frame();
-        roll(pins, frames[index]);
+    private boolean isSpare(Frame frame) {
+        return frame.pinsRolled[0] != 10 && frame.pinsRolled[0] + frame.pinsRolled[1] == 10;
+    }
+
+    void addRoll(int pins) throws Exception {
+        if (isOver()) throw new Exception();
+
+        int index;
+        if (rollIndex < 20) {
+            index = rollIndex / 2;
+            if (rollIndex % 2 == 0)
+                frames[index] = new Frame();
+        } else index = 9;
+
+        roll(pins, frames, index);
         rollIndex++;
     }
 
-    private void roll(int pins, Frame frame) {
+    private void roll(int pins, Frame[] frames, int index) {
+        if (index != 0 && frames[index - 1].score == 10) frames[index - 1].score += pins;
+
+        final Frame frame = frames[index];
+        putPinsRolledInFrame(pins, frame);
+    }
+
+    private void putPinsRolledInFrame(int pins, Frame frame) {
         if (frame.score == 0)
             frame.pinsRolled[0] = pins;
-        else
+        else if (frame.pinsRolled[1] == 0)
             frame.pinsRolled[1] = pins;
+        else frame.pinsRolled[2] = pins;
         frame.score += pins;
     }
 
     private class Frame {
-        final int[] pinsRolled = new int[2];
+        final int[] pinsRolled = new int[3];
         int score = 0;
 
 
--- Katas/src/Bowling/solution1/GameTest.java
+++ Katas/src/Bowling/solution1/GameTest.java
@@ -20,39 +20,62 @@ public class GameTest {
     }
 
     @Test
-    public void gameOver_after1Roll() {
+    public void gameOver_after1Roll() throws Exception {
         rollTimes(1, 0);
         assertEquals(false, bowlingGame.isOver());
     }
 
     @Test
-    public void TotalScore_after1Roll_given0() {
+    public void TotalScore_after1Roll_given0() throws Exception {
         rollTimes(1, 0);
         assertEquals(0, bowlingGame.totalScore());
     }
 
     @Test
-    public void TotalScore_after20Roll_given0() {
+    public void TotalScore_after20Roll_given0() throws Exception {
         rollTimes(20, 0);
         assertEquals(0, bowlingGame.totalScore());
     }
 
     @Test
-    public void gameOver_after20Rolls_given0() {
+    public void gameOver_after20Rolls_given0() throws Exception {
         rollTimes(20, 0);
         assertEquals(true, bowlingGame.isOver());
     }
 
-    private void rollTimes(int times, int pins) {
+    private void rollTimes(int times, int pins) throws Exception {
         for (int i = 0; i < times; i++) {
             bowlingGame.addRoll(pins);
         }
     }
 
     @Test
-    public void TotalScore_after20Roll_given1() {
+    public void TotalScore_after20Roll_given1() throws Exception {
         rollTimes(20, 1);
         assertEquals(20, bowlingGame.totalScore());
+    }
+
+    @Test
+    public void TotalScore_rollSpareAnd5() throws Exception {
+        rollTimes(3, 5);
+        assertEquals(20, bowlingGame.totalScore());
+    }
+
+    @Test
+    public void TotalScore_after18Rolls_given0_And3Rolls_given5() throws Exception {
+        rollTimes(18, 0);
+        rollTimes(3, 5);
+        assertEquals(15, bowlingGame.totalScore());
+    }
+
+    @Test(expected = Exception.class)
+    public void TotalScore_rollAfterGameOver() throws Exception {
+        rollTimes(21, 0);
+    }
+
+    @Test(expected = Exception.class)
+    public void TotalScore_roll22Spares() throws Exception {
+        rollTimes(22, 5);
     }
 
 
