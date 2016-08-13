8b2c133ecf109615fd4accdccce1addfb4e03e84
Gott50 <xxGott50xx@gmail.com>
2016-08-13 17:33:50 +0200

    day 5 BoxPlot

--- Katas/src/Box_Plot/solution1/GraphicalControl.java
+++ Katas/src/Box_Plot/solution1/GraphicalControl.java
@@ -29,17 +29,37 @@ class GraphicalControl {
     }
 
     String printNumberLine() {
+        char filler = '-';
+        char mark = '|';
+
+
+        String markStr = String.valueOf(mark);
+
         String out = "";
         for (int number = boxPlot.getMinimum();
              number <= boxPlot.getMaximum(); number++) {
-            if (number == boxPlot.getMaximum()) out += "|";
-            else
-                out += "|" +
-                        getCharTimesMaxLength('-') +
-                        "|" +
-                        getCharTimesMaxLength('-');
+            if (number == boxPlot.getMaximum())
+                out += markStr;
+            else {
+                out += markStr +
+                        getCharTimesMaxLength(filler) +
+                        markStr +
+                        getCharTimesMaxLength(filler);
+            }
         }
         return out;
+    }
+
+    private String generatePause(char pauseChar, float start, float end) {
+        return generatePause(pauseChar, pauseChar, start, end, pauseChar);
+    }
+
+    String printUpperEdge() {
+        return generateBoxPlotEdge('_');
+    }
+
+    String printLowerEdge() {
+        return generateBoxPlotEdge('‾');
     }
 
     private String generateBoxPlotEdge(char character) {
@@ -52,28 +72,21 @@ class GraphicalControl {
         return out;
     }
 
-    private String generatePause(char pauseChar, float start, float end) {
-        String out = String.valueOf(pauseChar);
-        for (float number = start;
-             number <= end; number += 0.5f) {
-            out += getCharTimesMaxLength(pauseChar);
-        }
-        return out;
-    }
-
-    String printUpperEdge() {
-        return generateBoxPlotEdge('_');
-    }
-
-    String printLowerEdge() {
-        return generateBoxPlotEdge('‾');
-    }
-
     String generateBoxPlotBody() {
-        return "|" + generatePause('-', boxPlot.getMinimum(), boxPlot.getLowerQuartile() - 1) + "|"
+        return generatePause('|', '-', boxPlot.getMinimum(), boxPlot.getLowerQuartile(), '|')
                 + generatePause(' ', boxPlot.getLowerQuartile(), boxPlot.getMedian() - 0.5f) + "|"
                 + generatePause(' ', boxPlot.getMedian(), boxPlot.getUpperQuartile() - 0.5f) + "|"
                 + generatePause('-', boxPlot.getUpperQuartile(), boxPlot.getMaximum() - 1) + "|";
+    }
+
+    private String generatePause(char startChar, char pauseChar, float start, float end, char endChar) {
+        String out =
+                String.valueOf(startChar) + getCharTimesMaxLength(pauseChar).substring(1);
+        for (float number = start;
+             number < end; number += 0.5f) {
+            out += pauseChar + getCharTimesMaxLength(pauseChar);
+        }
+        return out.substring(0, out.length() - 1) + endChar;
     }
 
     String printBoxPlot() {
--- Katas/src/Box_Plot/solution1/GraphicalControlTest.java
+++ Katas/src/Box_Plot/solution1/GraphicalControlTest.java
@@ -56,6 +56,7 @@ public class GraphicalControlTest {
         assertEquals(expected, graph.printBoxPlot());
     }
 
+
     @Test
     @Ignore
     public void integration() {
