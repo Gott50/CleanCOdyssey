b19e2e4ab260308f7591d421c1cb2afbe3c8c42a
Gott50 <xxGott50xx@gmail.com>
2017-11-08 09:04:01 +0100

    solve Problem 2

--- exercism/php/hamming/hamming.php
+++ exercism/php/hamming/hamming.php
@@ -10,4 +10,12 @@ function distance($a, $b)
     //
     // YOUR CODE GOES HERE
     //
+    if (strlen($a) != strlen($b))
+        throw new InvalidArgumentException("DNA strands must be of equal length.");
+
+    $out = 0;
+    for ($i = 0; $i < strlen($a); $i++)
+        if ($a[$i] != $b[$i]) $out++;
+
+    return $out;
 }
