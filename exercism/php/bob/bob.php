281c0cb63036fb17c4b75109c467da522f047a39
Gott50 <xxGott50xx@gmail.com>
2017-11-13 09:42:31 +0100

    Problem 4: pass basic Tests

--- exercism/php/bob/bob.php
+++ exercism/php/bob/bob.php
@@ -12,10 +12,22 @@ class Bob
 
     public function respondTo($string)
     {
-        if (preg_match("/\?$/", $string))
-            return "Sure.";
-        else if (preg_match("/!$/", $string) || preg_match("/[A-Z]$/", $string))
+        if (preg_match("/[A-Z]+/", $string)
+            && $this->sameAsUpperCase($string))
             return "Whoa, chill out!";
+        if (preg_match("/\?\s*$/", $string))
+            return "Sure.";
+        if (preg_match("/^\s*$/", $string))
+            return "Fine. Be that way!";
         return "Whatever.";
+    }
+
+    /**
+     * @param $string
+     * @return int
+     */
+    public function sameAsUpperCase($string): int
+    {
+        return strtoupper($string)==$string;
     }
 }
