eb234daac8a5ee89de641b3201812fb2cf9be892
Gott50 <xxGott50xx@gmail.com>
2017-11-22 08:55:27 +0100

    Problem 7: solved

--- exercism/php/run-length-encoding/run-length-encoding.php
+++ exercism/php/run-length-encoding/run-length-encoding.php
@@ -8,38 +8,44 @@
 function encode($input)
 {
     $pairs = [];
-    for ($i = 0; $i < strlen($input); $i++){
+    for ($i = 0; $i < strlen($input); $i++) {
         $lastChar = array_pop($pairs);
         if ($lastChar == null)
-            array_push($pairs,1,$input[$i]);
+            array_push($pairs, 1, $input[$i]);
         else {
             $lastCount = array_pop($pairs);
             if ($lastCount == null)
-                array_push($pairs,1, $input[$i]);
+                array_push($pairs, 1, $input[$i]);
             if ($lastChar == $input[$i])
-                array_push($pairs,$lastCount+1, $input[$i]);
+                array_push($pairs, $lastCount + 1, $input[$i]);
             else
-                array_push($pairs,$lastCount,$lastChar,1, $input[$i]);
+                array_push($pairs, $lastCount, $lastChar, 1, $input[$i]);
         }
     }
-    return implode('',array_filter($pairs, "filter_1"));
+    return implode('', array_filter($pairs, "filter_1"));
 }
 
-function filter_1($value){
+function filter_1($value)
+{
     return $value != 1;
 }
 
 function decode($input)
 {
     $out = "";
-    for($i = 0; $i < strlen($input); $i++){
-        $times = 1;
-        while (intval($input[$i])> 0){
-            $times = intval($input[$i]);
-            $i++;
-        }
-        for ($t = 0; $t < $times; $t++){
+    for ($i = 0; $i < strlen($input); $i++) {
+        if (intval($input[$i]) == 0) {
             $out .= $input[$i];
+            continue;
+        } else {
+            $times = "";
+            while (intval($input[$i]) > 0) {
+                $times .= $input[$i];
+                $i++;
+            }
+            for ($t = 0; $t < intval($times); $t++) {
+                $out .= $input[$i];
+            }
         }
     }
     return $out;
