30e96db5545bead08643e56f50b415bef0b49c78
Gott50 <xxGott50xx@gmail.com>
2017-12-02 16:50:46 +0100

    Problem 8: refactor solved

--- exercism/php/isogram/isogram.php
+++ exercism/php/isogram/isogram.php
@@ -1,15 +1,18 @@
 <?php
-function isIsogram($in){
+function isIsogram($in)
+{
     $in = strtolower($in);
-    $in = preg_replace("/-/","",$in);
-    $in = preg_replace("/\s/","",$in);
+    $in = preg_replace("/-/", "", $in);
+    $in = preg_replace("/\s/", "", $in);
 
     $spec = preg_split("/\w+/", $in);
-    $spec = array_filter($spec,function ($s) {return $s !== "";});
+    $spec = array_filter($spec, function ($s) {
+        return $s !== "";
+    });
 
-    $in = preg_replace("/\W/","",$in);
-    $chars =  str_split($in);
+    $in = preg_replace("/\W/", "", $in);
+    $chars = str_split($in);
 
-    return !(sizeof($spec) !== sizeof(array_unique($spec)))&&
-    !(sizeof($chars) !== sizeof(array_unique($chars)));
+    return !(sizeof($spec) !== sizeof(array_unique($spec))) &&
+        !(sizeof($chars) !== sizeof(array_unique($chars)));
 }
