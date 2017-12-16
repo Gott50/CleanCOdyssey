e18002824d333543af80571111f5fb793575fa2d
Gott50 <xxGott50xx@gmail.com>
2017-12-16 06:27:44 +0100

    Problem 16: refactor

--- exercism/php/word-count/word-count.php
+++ exercism/php/word-count/word-count.php
@@ -1,22 +1,23 @@
 <?php
-function wordCount($sentence)
-{
+function wordCount( $sentence ) {
 	$sentence = strtolower( $sentence );
-	$sentence = str_replace(array("\n", "\r", "\t", "\u000b", "\u00a0", "\u2002"), " ", $sentence);
-	$sentence = preg_filter( "/[^a-z\d ]/", "", "!".$sentence);
+	$sentence = str_replace( array( "\n", "\r", "\t", "\u000b", "\u00a0", "\u2002" ), " ", $sentence );
+	$sentence = preg_filter( "/[^a-z\d ]/", "", "!" . $sentence );
 
-	$words = preg_split("/ +/", $sentence);
-    $words = array_filter($words, function ( $e ) {
-	   return $e != "";
-    });
+	$words = preg_split( "/ +/", $sentence );
+	$words = array_filter( $words, function ( $e ) {
+		return $e != "";
+	} );
 	$words = array_values( $words );
 
-    $unique = array();
-    for ($i = 0; $i < sizeof($words); $i++)
-        if (array_key_exists($words[$i], $unique))
-            $unique[$words[$i]]++;
-        else
-	        $unique[ $words[ $i ] ] = 1;
+	$unique = array();
+	for ( $i = 0; $i < sizeof( $words ); $i ++ ) {
+		if ( array_key_exists( $words[ $i ], $unique ) ) {
+			$unique[ $words[ $i ] ] ++;
+		} else {
+			$unique[ $words[ $i ] ] = 1;
+		}
+	}
 
-    return $unique;
+	return $unique;
 }
