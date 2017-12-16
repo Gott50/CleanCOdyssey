ac66fec4955e15089d3953bb52254de101dfa3f1
Gott50 <xxGott50xx@gmail.com>
2017-12-16 06:33:25 +0100

    Problem 16: refactor

--- exercism/php/word-count/word-count.php
+++ exercism/php/word-count/word-count.php
@@ -10,14 +10,11 @@ function wordCount( $sentence ) {
  */
 function countOccurrences( $words ): array {
 	$unique = array();
-	for ( $i = 0; $i < sizeof( $words ); $i ++ ) {
-		if ( array_key_exists( $words[ $i ], $unique ) ) {
+	for ( $i = 0; $i < sizeof( $words ); $i ++ )
+		if ( array_key_exists( $words[ $i ], $unique ) )
 			$unique[ $words[ $i ] ] ++;
-		} else {
+		 else
 			$unique[ $words[ $i ] ] = 1;
-		}
-	}
-
 	return $unique;
 }
 
@@ -31,9 +28,7 @@ function extractWords( $sentence ) {
 	$words = array_filter( $words, function ( $e ) {
 		return $e != "";
 	} );
-	$words = array_values( $words );
-
-	return $words;
+	return array_values( $words );
 }
 
 /**
@@ -45,6 +40,5 @@ function cleanup( $sentence ) {
 	$sentence = strtolower( $sentence );
 	$sentence = str_replace( array( "\n", "\r", "\t", "\u000b", "\u00a0", "\u2002" ), " ", $sentence );
 	$sentence = preg_filter( "/[^a-z\d ]/", "", "!" . $sentence );
-
 	return $sentence;
 }
