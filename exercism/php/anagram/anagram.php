1ffeb0167be6b6b5e1927bb5fc0cab88017ab985
Gott50 <xxGott50xx@gmail.com>
2017-12-20 08:59:39 +0100

    Problem 17: refactor

--- exercism/php/anagram/anagram.php
+++ exercism/php/anagram/anagram.php
@@ -1,16 +1,19 @@
 <?php
 function detectAnagrams( $word, $list ) {
-	return array_values(array_filter($list,function ($e) use ( $word ) {
-		$e = mb_strtolower( $e );
+	return array_values( array_filter( $list, function ( $e ) use ( $word ) {
+		$e    = mb_strtolower( $e );
 		$word = mb_strtolower( $word );
-		if (strlen($e) != strlen($word)||$e == $word) return false;
+		if ( strlen( $e ) != strlen( $word ) || $e == $word ) {
+			return false;
+		}
 
-		$difference = array_reduce(str_split( $word ),function ($carry , $item){
-			$index = array_search($item, $carry);
-			unset( $carry[$index] );
+		$difference = array_reduce( str_split( $word ), function ( $carry, $item ) {
+			$index = array_search( $item, $carry );
+			unset( $carry[ $index ] );
+
 			return $carry;
-		},str_split($e));
+		}, str_split( $e ) );
 
 		return sizeof( $difference ) == 0;
-	}));
+	} ) );
 }
