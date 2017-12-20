0d17340b0305d11264b1e3da008ceeb514fd6a46
Gott50 <xxGott50xx@gmail.com>
2017-12-20 08:58:52 +0100

    Problem 17: solved

--- exercism/php/anagram/anagram.php
+++ exercism/php/anagram/anagram.php
@@ -1,8 +1,8 @@
 <?php
 function detectAnagrams( $word, $list ) {
 	return array_values(array_filter($list,function ($e) use ( $word ) {
-		$e = strtolower( $e );
-		$word = strtolower( $word );
+		$e = mb_strtolower( $e );
+		$word = mb_strtolower( $word );
 		if (strlen($e) != strlen($word)||$e == $word) return false;
 
 		$difference = array_reduce(str_split( $word ),function ($carry , $item){
