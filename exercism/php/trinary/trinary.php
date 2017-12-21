bdcc2ceae7044d9f5d84e8b6d96fbdc33500e169
Gott50 <xxGott50xx@gmail.com>
2017-12-21 14:07:41 +0100

    Problem 18: solved

---
+++ exercism/php/trinary/trinary.php
@@ -0,0 +1,16 @@
+<?php
+function toDecimal( $string ) {
+	if(!isValid($string)) return 0;
+
+	$out =0;
+	for ($i = 0; $i < strlen($string); $i++){
+		$out += intval($string[$i])*pow(3,(strlen($string)-$i-1));
+
+	}
+
+	return $out;
+}
+
+function isValid( $string ) {
+	return	preg_match( "/^[0-2]+$/", $string );
+}
