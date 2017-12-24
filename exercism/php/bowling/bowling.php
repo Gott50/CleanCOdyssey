d90ef4e0cb7d8ccc6be672535ccc0bb623c8c013
Gott50 <xxGott50xx@gmail.com>
2017-12-24 13:23:52 +0100

    Problem 19: refactor

--- exercism/php/bowling/bowling.php
+++ exercism/php/bowling/bowling.php
@@ -4,59 +4,67 @@ class Game {
 	private $rolls = array();
 
 	public function score() {
-		if (sizeof($this->rolls) <= 2)
+		if ( sizeof( $this->rolls ) <= 2 ) {
 			throw new Exception();
+		}
 
-
-		$frames =[];
-		$next = true;
+		$frames = [];
+		$next   = true;
 		for ( $i = 0; $i < sizeof( $this->rolls ); $i ++ ) {
-				$roll = $this->rolls[ $i ];
+			$roll = $this->rolls[ $i ];
 
 			if ( $next ) {
 				array_push( $frames, $roll );
-				if ( $frames[ sizeof( $frames ) - 1 ] == 10) {
+				if ( $frames[ sizeof( $frames ) - 1 ] == 10 ) {
 					echo $i;
 
 					if ( sizeof( $frames ) <= 10 ) {
-					if ($i + 1<sizeof( $this->rolls  ))
-					$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 1 ];
-					else
+						if ( $i + 1 < sizeof( $this->rolls ) ) {
+							$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 1 ];
+						} else {
+							throw new Exception();
+						}
+						if ( $i + 2 < sizeof( $this->rolls ) ) {
+							$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 2 ];
+						}
+						$next = ! $next;
+					}
+				}
+			} else {
+				if ( $frames[ sizeof( $frames ) - 1 ] < 10
+				     && $frames[ sizeof( $frames ) - 1 ] + $roll > 10 ) {
 					throw new Exception();
-					if ($i + 2<sizeof( $this->rolls  ))
-					$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 2 ];
-					$next = !$next;
+				}
+				$frames[ sizeof( $frames ) - 1 ] += $roll;
+
+				if ( $frames[ sizeof( $frames ) - 1 ] == 10 ) {
+					if ( $i + 1 < sizeof( $this->rolls ) ) {
+						$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 1 ];
 					}
 				}
 			}
-			else{
-				if ($frames[ sizeof( $frames ) - 1 ] < 10
-				    &&  $frames[ sizeof( $frames ) - 1 ] + $roll > 10 )
-					throw new Exception();
-				$frames[ sizeof( $frames ) - 1 ] += $roll;
-
-				if ($frames[ sizeof( $frames ) - 1 ] == 10)
-					if ($i + 1<sizeof( $this->rolls  ))
-					$frames[ sizeof( $frames ) - 1 ] += $roll = $this->rolls[ $i + 1 ];
-			}
-			$next = !$next;
+			$next = ! $next;
 		}
 
-		echo var_dump($frames);
-		if (sizeof($frames) == 11 && $frames[9] < 10)
+		echo var_dump( $frames );
+		if ( sizeof( $frames ) == 11 && $frames[9] < 10 ) {
 			throw new Exception();
+		}
 
 
-		if(sizeof($frames) == 11)
+		if ( sizeof( $frames ) == 11 ) {
 			unset( $frames[10] );
-		return  array_reduce($frames,function ($carry, $item){
-			return $carry+$item;
-		},0);
+		}
+
+		return array_reduce( $frames, function ( $carry, $item ) {
+			return $carry + $item;
+		}, 0 );
 	}
 
 	public function roll( $int ) {
-		if ($int < 0 || $int > 10)
+		if ( $int < 0 || $int > 10 ) {
 			throw new Exception();
+		}
 
 		array_push( $this->rolls, $int );
 	}
