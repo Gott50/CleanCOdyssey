54e9ad218ad3e2004f016b846147fb13d2ac0ff0
Gott50 <xxGott50xx@gmail.com>
2018-01-17 09:09:38 +0100

    Problem 24: solved

--- exercism/php/change/change.php
+++ exercism/php/change/change.php
@@ -1,12 +1,43 @@
 <?php
 function findFewestCoins( $coins, $change ) {
-	return findCoins( $coins, $change );
+	if ( $change == 0 ) {
+		return array();
+	}
+	if ( $change < 0 ) {
+		throw new InvalidArgumentException( 'Cannot make change for negative value' );
+	}
+	if ( $change < $coins[0] ) {
+		throw new InvalidArgumentException( 'No coins small enough to make change' );
+	}
+
+	$solutions = calculateSolutions( $coins, $change );
+	sort( $solutions );
+
+	return $solutions[0];
 }
 
 /**
  * @param $coins
  * @param $change
- * @param $solutions
+ *
+ * @return array
+ */
+function calculateSolutions( $coins, $change ): array {
+	$solutions = [];
+	while ( sizeof( $coins ) >= 1 ) {
+		$sol = findCoins( $coins, $change );
+		if ( sizeof( $sol ) >= 1 ) {
+			array_push( $solutions, $sol );
+		}
+		unset( $coins[ sizeof( $coins ) - 1 ] );
+	}
+
+	return $solutions;
+}
+
+/**
+ * @param $coins
+ * @param $change
  *
  * @return array
  */
--- exercism/php/change/change_test.php
+++ exercism/php/change/change_test.php
@@ -16,19 +16,16 @@ class ChangeTest extends PHPUnit\Framework\TestCase
 
     public function testChangeWithLilliputianCoins()
     {
-        $this->markTestSkipped();
         $this->assertEquals(array(4, 4, 15), findFewestCoins(array(1, 4, 15, 20, 50), 23));
     }
 
     public function testChangeWithLowerElboniaCoins()
     {
-        $this->markTestSkipped();
         $this->assertEquals(array(21, 21, 21), findFewestCoins(array(1, 5, 10, 21, 25), 63));
     }
 
     public function testWithLargeTargetValue()
     {
-        $this->markTestSkipped();
         $this->assertEquals(
             array(2, 2, 5, 20, 20, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100),
             findFewestCoins(array(1, 2, 5, 10, 20, 50, 100), 999)
@@ -37,20 +34,17 @@ class ChangeTest extends PHPUnit\Framework\TestCase
 
     public function testForChangeSmallerThanAvailableCoins()
     {
-        $this->markTestSkipped();
         $this->expectException('InvalidArgumentException', 'No coins small enough to make change');
         findFewestCoins(array(5, 10), 3);
     }
 
     public function testNoCoinsForZero()
     {
-        $this->markTestSkipped();
         $this->assertEquals(array(), findFewestCoins(array(1, 2, 5), 0));
     }
 
     public function testChangeValueLessThanZero()
     {
-        $this->markTestSkipped();
         $this->expectException('InvalidArgumentException', 'Cannot make change for negative value');
         findFewestCoins(array(1, 2, 5), -5);
     }
