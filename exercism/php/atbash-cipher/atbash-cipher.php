7cfdb0ae192621c9e04e998ffeb4ac99fce9d770
Gott50 <xxGott50xx@gmail.com>
2018-01-23 07:11:32 +0100

    Problem 27: refactor

--- exercism/php/atbash-cipher/atbash-cipher.php
+++ exercism/php/atbash-cipher/atbash-cipher.php
@@ -1,27 +1,27 @@
 <?php
 function encode( $input ) {
-	$en  = function ( $in ) {
-		if ( preg_match( "/\d/", $in ) ) {
-			return $in;
-		}
+	$cleaned = preg_replace( "/\W/", "", $input . " " );
+	$encoded = array_map( "encodeLetter", str_split( $cleaned ) );
+	$split   = array_reduce( $encoded, "splitAtLength5", [] );
 
-		return chr( ord( 'a' ) + ord( 'z' ) - ord( strtolower( $in ) ) );
-	};
-	$red = function ( $carry, $item ) {
-		$s = sizeof( $carry );
-		if ( $s <= 0 || strlen( $carry[ $s - 1 ] ) >= 5 ) {
-			array_push( $carry, $item );
-		} else {
-			$carry[ $s - 1 ] .= $item;
-		}
+	return implode( " ", $split );
+}
 
-		return $carry;
-	};
+function encodeLetter( $in ) {
+	if ( preg_match( "/\d/", $in ) ) {
+		return $in;
+	}
 
+	return chr( ord( 'a' ) + ord( 'z' ) - ord( strtolower( $in ) ) );
+}
 
-	$input = preg_replace( "/\W/", "", $input . " " );
+function splitAtLength5( $carry, $item ) {
+	$s = sizeof( $carry );
+	if ( $s <= 0 || strlen( $carry[ $s - 1 ] ) >= 5 ) {
+		array_push( $carry, $item );
+	} else {
+		$carry[ $s - 1 ] .= $item;
+	}
 
-	$pieces = array_reduce( array_map( $en, str_split( $input ) ), $red, [] );
-
-	return implode( " ", $pieces );
+	return $carry;
 }
