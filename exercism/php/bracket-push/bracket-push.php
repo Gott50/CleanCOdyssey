0661dbee1ed58a57bedb65fa381f517603398aa7
Gott50 <xxGott50xx@gmail.com>
2018-01-24 06:40:01 +0100

    Problem 28: refactor

--- exercism/php/bracket-push/bracket-push.php
+++ exercism/php/bracket-push/bracket-push.php
@@ -3,15 +3,35 @@
 function brackets_match( $input ) {
 	$stack = [];
 	for ( $i = 0; $i < strlen( $input ); $i ++ ) {
-		if ( $input[ $i ] == "[" || $input[ $i ] == "{" || $input[ $i ] == "(" ) {
+		if ( isOpeninBracket( $input, $i ) ) {
 			array_push( $stack, $input[ $i ] );
-		} elseif ( ( $input[ $i ] == "]" || $input[ $i ] == "}" || $input[ $i ] == ")" )
-		           && ( ! match( array_pop( $stack ), $input[ $i ] ) ) ) {
+		} elseif ( isClosingBracket( $input, $i )
+		           && ! match( array_pop( $stack ), $input[ $i ] ) ) {
 			return false;
 		}
 	}
 
 	return sizeof( $stack ) == 0;
+}
+
+/**
+ * @param $input
+ * @param $i
+ *
+ * @return bool
+ */
+function isClosingBracket( $input, $i ): bool {
+	return $input[ $i ] == "]" || $input[ $i ] == "}" || $input[ $i ] == ")";
+}
+
+/**
+ * @param $input
+ * @param $i
+ *
+ * @return bool
+ */
+function isOpeninBracket( $input, $i ): bool {
+	return $input[ $i ] == "[" || $input[ $i ] == "{" || $input[ $i ] == "(";
 }
 
 function match( $a, $b ) {
