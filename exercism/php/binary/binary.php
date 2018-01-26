8522dd29fb7cba0b861343ac99bde4adfb5a2b7e
Gott50 <xxGott50xx@gmail.com>
2018-01-26 07:27:31 +0100

    Problem 29: solved

--- exercism/php/binary/binary.php
+++ exercism/php/binary/binary.php
@@ -6,7 +6,18 @@
 // function should raise an \InvalidArgumentException
 // with a meaningful error message.
 
-function parse_binary($binary)
-{
-    // YOUR CODE GOES HERE
+function parse_binary( $binary ) {
+	if(! isValid($binary))
+		throw new InvalidArgumentException();
+
+	$out = 0;
+	for ( $i = strlen( $binary ) - 1, $p = 0; $i >= 0; $i --, $p ++ ) {
+		$out += $binary[ $i ] * pow( 2, $p );
+	}
+
+	return $out;
+}
+
+function isValid( $binary ) {
+	return preg_match( "/^[0,1]+$/", $binary );
 }
