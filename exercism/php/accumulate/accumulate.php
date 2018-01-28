9badc77ff1d5ec8718f4af8bbd198223dbaffabf
Gott50 <xxGott50xx@gmail.com>
2018-01-28 10:58:03 +0100

    Problem 30: solved

--- exercism/php/accumulate/accumulate.php
+++ exercism/php/accumulate/accumulate.php
@@ -1,6 +1,9 @@
 <?php
 
-function accumulate(array $input, callable $accumulator)
-{
-    // YOUR CODE GOES HERE
+function accumulate( array $input, callable $accumulator ) {
+	for ( $i = 0; $i < sizeof( $input ); $i ++ ) {
+		$input[ $i ] = $accumulator( $input[ $i ] );
+	}
+
+	return $input;
 }
