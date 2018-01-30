e3102478b86d6dd403af764f1e12fc47473c987a
Gott50 <xxGott50xx@gmail.com>
2018-01-30 06:50:16 +0100

    Problem 31: refactor

--- exercism/php/variable-length-quantity/variable-length-quantity.php
+++ exercism/php/variable-length-quantity/variable-length-quantity.php
@@ -1,10 +1,20 @@
 <?php
 
 function vlq_encode( $input ) {
-	$out    = array_map( function ( $b ) {
-		return 0b10000000 | bindec( $b );
-	}, array_reverse( str_split( decbin( $input[0] ), 7 ) ) );
-	$out[0] &= 0b01111111;
+	$out                 = array_map( function ( $b ) {
+		return ( 0b10000000 | bindec( $b ));
+	}, split( decbin( $input[0] )) );
+	$out[sizeof($out)-1] &= 0b01111111;
 
-	return array_reverse( $out );
+	return ( $out );
+}
+
+function split( $str ) {
+	$out = array();
+	while ( strlen( $str ) > 0 ) {
+		array_push( $out, substr( $str, 0, 7 ) );
+		$str = substr( $str, 7 );
+	}
+
+	return $out;
 }
