0c3df2a2192089df687c696e73b5e3d3f7bf2230
Gott50 <xxGott50xx@gmail.com>
2018-02-08 17:19:19 +0100

    Problem 35: refactor

--- exercism/php/etl/etl.php
+++ exercism/php/etl/etl.php
@@ -2,47 +2,11 @@
 
 function transform( $old ) {
 	$out = [];
-	foreach ( $old as $e ) {
-		foreach ( $e as $item ) {
-			$out[ strtolower( $item ) ] = fill( strtolower( $item ) );
+	foreach ( $old as $key => $value) {
+		foreach ( $value as $item ) {
+			$out[ strtolower( $item ) ] = $key;
 		}
 	}
 
 	return $out;
-}
-
-/**
- * @param $key
- *
- * @return int
- */
-function fill( $key ): int {
-	return [
-		'a' => 1,
-		'b' => 3,
-		'c' => 3,
-		'd' => 2,
-		'e' => 1,
-		'f' => 4,
-		'g' => 2,
-		'h' => 4,
-		'i' => 1,
-		'j' => 8,
-		'k' => 5,
-		'l' => 1,
-		'm' => 3,
-		'n' => 1,
-		'o' => 1,
-		'p' => 3,
-		'q' => 10,
-		'r' => 1,
-		's' => 1,
-		't' => 1,
-		'u' => 1,
-		'v' => 4,
-		'w' => 4,
-		'x' => 8,
-		'y' => 4,
-		'z' => 10
-	][ $key ];
 }
