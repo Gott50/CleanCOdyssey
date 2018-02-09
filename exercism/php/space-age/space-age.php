10ad70c42ffd26f472d10e32e19ac765363223ea
Gott50 <xxGott50xx@gmail.com>
2018-02-09 07:11:27 +0100

    Problem 36: refactor

--- exercism/php/space-age/space-age.php
+++ exercism/php/space-age/space-age.php
@@ -4,28 +4,37 @@ class SpaceAge {
 	private $seconds;
 	private $planets = [
 		"mercury" => 0.2408467,
-		"venus" => 0.61519726,
-		"mars" => 1.8808158,
+		"venus"   => 0.61519726,
+		"mars"    => 1.8808158,
 		"jupiter" => 11.862615,
-		"saturn" => 29.447498,
-		"uranus" => 84.016846,
+		"saturn"  => 29.447498,
+		"uranus"  => 84.016846,
 		"neptune" => 164.79132,
 	];
 
-	public function __construct($seconds) {
+	public function __construct( $seconds ) {
 		$this->seconds = $seconds;
 	}
 
-	public function seconds() {
-		return $this->seconds;
+	public function mercury() {
+		return $this->calculateYears( $this->planets["mercury"] );
+	}
+
+	/**
+	 * @param $quotient
+	 *
+	 * @return float|int
+	 */
+	public function calculateYears( $quotient ) {
+		return $this->earth() / $quotient;
 	}
 
 	public function earth() {
 		return $this->seconds() / 31557600;
 	}
 
-	public function mercury() {
-		return $this->calculateYears( $this->planets["mercury"] );
+	public function seconds() {
+		return $this->seconds;
 	}
 
 	public function venus() {
@@ -50,14 +59,5 @@ class SpaceAge {
 
 	public function neptune() {
 		return $this->calculateYears( $this->planets["neptune"] );
-	}
-
-	/**
-	 * @param $quotient
-	 *
-	 * @return float|int
-	 */
-	public function calculateYears( $quotient ) {
-		return $this->earth() / $quotient;
 	}
 }
