86f49b6fba6dd25ef612fcb5a2dfa3379d756d74
Gott50 <xxGott50xx@gmail.com>
2018-02-18 07:35:17 +0100

    Problem 39: in progress

--- exercism/php/grains/grains.php
+++ exercism/php/grains/grains.php
@@ -5,4 +5,25 @@ function square( $index ) {
 	if ( ! is_float( $out ) ) {
 		return $out . "";
 	}
+
+	$int_size = PHP_INT_SIZE * 8 - 2;
+	$out      = 2 ** ( $int_size ) . "";
+	for ( $i = $int_size; $i < $index - 1; $i ++ ) {
+		$out = double( $out );
+	}
+
+	return $out;
+}
+
+function double( $number ) {
+	$out = "";
+	for ( $i = 0; $i < strlen( $number ); $i ++ ) {
+		$d = $number[ $i ] * 2;
+		if ( $d >= 10 && strlen( $out ) >= 0 ) {
+			$out[ strlen( $out ) - 1 ] = $out[ strlen( $out ) - 1 ] + 1;
+		}
+		$out .= $d % 10;
+	}
+
+	return $out;
 }
