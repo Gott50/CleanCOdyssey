6893f72989568a625796cc83c9889c2a16aed016
Gott50 <xxGott50xx@gmail.com>
2018-03-10 17:54:32 +0100

    Problem 45: refactor

--- exercism/php/grade-school/grade-school.php
+++ exercism/php/grade-school/grade-school.php
@@ -1,14 +1,7 @@
 <?php
 
 class School {
-	private $grades = [
-		1 => [],
-		2 => [],
-		3 => [],
-		4 => [],
-		5 => [],
-		6 => [],
-	];
+	private $grades = [];
 
 	function numberOfStudents() {
 		return array_reduce( $this->grades, function ( $acc, $g ) {
@@ -19,11 +12,16 @@ class School {
 	function add( $name, $grade ) {
 		if ( array_key_exists( $grade, $this->grades ) ) {
 			array_push( $this->grades[ $grade ], $name );
+		} else {
+			$this->grades[ $grade ] = [ $name ];
 		}
+
 	}
 
 	function grade( $grade ) {
-		return $this->grades[ $grade ];
+		if ( array_key_exists( $grade, $this->grades ) ) {
+			return $this->grades[ $grade ];
+		}
 	}
 
 	function studentsByGradeAlphabetical() {
